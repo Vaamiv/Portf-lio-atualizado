076b8f66a8f4bc2f60d3af50455f98bb293d13cf
Vaamiv <thomasmoojen94@hotmail.com>
2024-03-28 14:55:41 -0300

    agencia TM

---
+++ envia.php
@@ -0,0 +1,22 @@
+<?php
+
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone = addslashes($_POST['telefone']);
+    $mensagem = addslashes($_POST['mensagem']);
+    
+
+    $para ="thomasmoojen94@hotmail.com";
+    $assunto = "Coleta de dados - Agência TM";
+
+    $corpo ="Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
+
+    $cabeca = "From: thomasmoojen94@hotmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if (mail($para,$assunto,$corpo,$cabeca,$mensagem)) {
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("Houve um erro ao enviar o email!");
+    }
+
+>
